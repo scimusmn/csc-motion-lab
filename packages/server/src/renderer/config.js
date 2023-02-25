287fb883c0feb0795fba41d89347777bb856a1ef
Trygve Nordberg <tnordberg@smm.org>
2023-02-25 17:57:00 -0600

    feat: updating config vars for new cam approach

--- packages/server/src/renderer/config.js
+++ packages/server/src/renderer/config.js
@@ -1,8 +1,9 @@
 exports.config = {
-  recordTime: 3000, //ms
+  recordTime: 3000, // ms
   frameRate: 200,
   numImage: 800,
   imageGain: 3.99,
-  portName:'/dev/ttyUSB0',
   setsToStore: 16,
+  pathToCamera: '/home/pi/photobooth/camera.sh',
+  fileSaveDelay: 10 * 1000, // ms to wait after cam script runs to assume files have finished saving
 };
