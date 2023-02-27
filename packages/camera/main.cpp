58881a0d5607f64b4db860bf809c4070a484e260
sanine-a <sanine.not@pm.me>
2023-02-27 17:29:51 -0600

    fix bug with buffer overflow

--- packages/camera/main.cpp
+++ packages/camera/main.cpp
@@ -150,7 +150,7 @@ static void write(unsigned char byte, void *userdata) { fputc(byte, (FILE*) user
 // write Image to disk, called from thread pool
 void write_image(int unused, const char *prefix, Image img) {
 	// compute filename as [prefix] + [number].jpg
-	char filename[32];
+	char filename[1024];
 	snprintf(filename, 32, "%s%03d.jpg", prefix, img.index);
 
 	// open file
