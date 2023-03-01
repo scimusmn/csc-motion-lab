11ddc8947f643b2b91b0d0349a576093ff6fc83a
sanine-a <sanine.not@pm.me>
2023-03-01 17:16:59 -0600

    add port watching

--- packages/arduino/motion-lab-arduino/motion-lab-arduino.ino
+++ packages/arduino/motion-lab-arduino/motion-lab-arduino.ino
@@ -9,16 +9,61 @@ SerialController<> serial;
 long baudrate = 115200;
 
 
+class WatchedPin {
+	protected:
+	bool isHead;
+	unsigned int pin;
+	bool prevState;
+	WatchedPin *next;
+
+	public:
+	WatchedPin(int pin, bool head=false) 
+	: pin(pin), prevState(true), isHead(head), next(nullptr) {}
+
+	void update() {
+		if (!isHead) {
+			// check if state is changed
+			bool state = digitalRead(pin);
+			if (state != prevState) {
+				prevState = state;
+				if (state == true) {
+					serial.send("go-high", (int)pin);
+				} else {
+					serial.send("go-low", (int)pin);
+				}
+			}
+		}
+
+
+		// update children
+		if (next != nullptr) {
+			next->update();
+		}
+	}
+
+	void addWatch(int pin) {
+		if (next == nullptr) {
+			next = new WatchedPin(pin);
+		} else {
+			next->addWatch(pin);
+		}
+	}
+} watch(0, true);
+
+
 
 void configureOutput(int pin) {
+  serial.send("output-configured", pin);
 	pinMode(pin, OUTPUT);
 }
 
 void configureInput(int pin) {
+  serial.send("input-configured", pin);
 	pinMode(pin, INPUT);
 }
 
 void configurePullup(int pin) {
+  serial.send("pullup-configured", pin);
 	pinMode(pin, INPUT_PULLUP);
 }
 
@@ -28,25 +73,34 @@ void digital_read(int pin) {
 }
 
 void writeLow(int pin) {
+  serial.send("writing-low", pin);
 	digitalWrite(pin, 0);
 }
 
 void writeHigh(int pin) {
+  serial.send("writing-high", pin);
 	digitalWrite(pin, 1);
+}
+
+void watchPin(int pin) {
+  serial.send("watching", pin);
+	watch.addWatch(pin);
 }
 
 
 void setup() {
 	serial.setup(baudrate);
-	seiral.addCallback("configure-output", configureOutput);
+	serial.addCallback("configure-output", configureOutput);
 	serial.addCallback("configure-input", configureInput);
 	serial.addCallback("configure-pullup", configurePullup);
 	serial.addCallback("digital-read", digital_read);
 	serial.addCallback("write-low", writeLow);
 	serial.addCallback("write-high", writeHigh);
+  serial.addCallback("watch-pin", watchPin);
 }
 
 
 void loop() {
-	serialController.update();
+	serial.update();
+	watch.update();
 }
