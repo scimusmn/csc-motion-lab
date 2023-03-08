0e7719b1e59d8962ae5200e0e88c710d4eb481c8
Trygve Nordberg <tnordberg@smm.org>
2023-03-08 16:03:47 -0600

    feat: passing cam gain setting into camera exe

--- packages/server/src/renderer/config.js
+++ packages/server/src/renderer/config.js
@@ -2,7 +2,7 @@ exports.config = {
   recordTime: 3000, // ms
   frameRate: 200,
   numImage: 800,
-  imageGain: 3.99,
+  cameraGain: 20,
   setsToStore: 16,
   pathToCamera: '../camera/out/build/x64-Release/camera-capture.exe',
   fileSaveDelay: 10 * 1000, // ms to wait after cam script runs to assume files have finished saving
