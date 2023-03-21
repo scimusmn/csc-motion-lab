af61f89e0a4c2e7440feb225df31b785314bc122
sanine-a <sanine.not@pm.me>
2023-03-21 14:49:57 -0500

    add more comments to motion-lab-arduino.ino

--- packages/arduino/motion-lab-arduino/motion-lab-arduino.ino
+++ packages/arduino/motion-lab-arduino/motion-lab-arduino.ino
@@ -10,6 +10,9 @@ long baudrate = 115200;
 
 #define WATCH_DEBOUNCE 10
 
+
+// class for storing pin watches
+// new watches are added on to the HEAD watch as a linked list
 class WatchedPin {
 	protected:
 	bool isHead;
@@ -22,7 +25,7 @@ class WatchedPin {
 	public:
 	WatchedPin(int pin, bool head=false) 
 	: pin(pin), isHead(head) {
-		prevState = true;
+		prevState = true; // most inputs will be pulled up by default
 		next = nullptr;
 		debouncing = false;
 		debounceEnd = 0;
@@ -30,11 +33,12 @@ class WatchedPin {
 
 	void update() {
 		if (!isHead && !debouncing) {
-			// check if state is changed
+			// check if state is changed (HEAD will never do this)
 			bool state = digitalRead(pin);
 			if (state != prevState) {
+				// we just changed
 				debouncing = true;
-				debounceEnd = millis() + WATCH_DEBOUNCE;
+				debounceEnd = millis() + WATCH_DEBOUNCE; // stop debouncing after WATCH_DEBOUNCE milliseconds
 				prevState = state;
 				if (state == true) {
 					serial.send("go-high", (int)pin);
@@ -49,7 +53,7 @@ class WatchedPin {
 			debouncing = false;
 		}
 
-		// update children
+		// recursively update children
 		if (next != nullptr) {
 			next->update();
 		}
@@ -57,25 +61,30 @@ class WatchedPin {
 
 	void addWatch(int pin) {
 		if (next == nullptr) {
+			// this is the last element in the list, append a new watch
 			next = new WatchedPin(pin);
 		} else {
+			// recursively traverse the list until we get to the end
 			next->addWatch(pin);
 		}
 	}
-} watch(0, true);
+} watch(0, true); // create a HEAD watch (whose update() will only update the children in the list)
 
 
 
+// configure a pin as an output
 void configureOutput(int pin) {
 	serial.send("output-configured", pin);
 	pinMode(pin, OUTPUT);
 }
 
+// configure a pin as an input
 void configureInput(int pin) {
 	serial.send("input-configured", pin);
 	pinMode(pin, INPUT);
 }
 
+// configure a pin as an input with pullup resistor
 void configurePullup(int pin) {
 	serial.send("pullup-configured", pin);
 	pinMode(pin, INPUT_PULLUP);
@@ -96,6 +105,7 @@ void writeHigh(int pin) {
 	digitalWrite(pin, 1);
 }
 
+// add a watch to on a pin
 void watchPin(int pin) {
 	serial.send("watching", pin);
 	watch.addWatch(pin);
@@ -104,6 +114,8 @@ void watchPin(int pin) {
 
 void setup() {
 	serial.setup(baudrate);
+
+	// attach the callback functions to string keys
 	serial.addCallback("configure-output", configureOutput);
 	serial.addCallback("configure-input", configureInput);
 	serial.addCallback("configure-pullup", configurePullup);
@@ -115,6 +127,8 @@ void setup() {
 
 
 void loop() {
+	// check for serial messages
 	serial.update();
+	// check for watched pin state changes
 	watch.update();
 }
